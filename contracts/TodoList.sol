3a17d0428b8dd5614130b5cdc1e7ce098c1c9d0b
Sebastián Mena <smenaaliaga@gmail.com>
2021-07-14 09:12:57 -0400

    first commit

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,5 +3,22 @@
 pragma solidity >=0.5.16 <0.9.0;
 
 contract TodoList {
-    uint public taskCount;
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Terminar TodoList Blockchain");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const TodoList = artifacts.require("TodoList");
+const Migrations = artifacts.require("Migrations");
 
 module.exports = function (deployer) {
-  deployer.deploy(TodoList);
+  deployer.deploy(Migrations);
 };
